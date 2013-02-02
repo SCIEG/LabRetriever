e42f896dab7ca2c4a8e470948fcec1eba09ad31b
Luke Inman-Semerau <luke.semerau@gmail.com>
2013-02-01 22:31:59 -0800

    fixing logger loader

--- src/Tide/Resources/js/logger.js
+++ src/Tide/Resources/js/logger.js
@@ -1,9 +1,10 @@
+
 var file = Ti.Filesystem.getFile(Ti.Filesystem.getApplicationDataDirectory(), "user.properties");
 var userProperties;
 
 //if file exists, then load properties.
 if(file.exists()) {
-    userProperties = Ti.App.loadProperties(file);
+    userProperties = Ti.App.loadProperties(file.nativePath());
 } else {
 //create new set of properties if file doesn't exist
     userProperties = Ti.App.createProperties({
@@ -15,6 +16,7 @@ userProperties.saveTo(file.nativePath());
 file = null;
 
 function log(msg) {
-    if (userProperties.getString('debug') == 'true')
+    if (userProperties.getString('debug') == 'true') {
         $("#status").append(msg).append('<br/>');
+    }
 }
