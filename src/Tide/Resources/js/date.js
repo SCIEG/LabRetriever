e2080d2cdd716dfa39badf2c7d0ffdb5da052de6
Luke Inman-Semerau <luke.semerau@gmail.com>
2013-02-16 12:42:37 -0800

    fixing month in date format

--- src/Tide/Resources/js/date.js
+++ src/Tide/Resources/js/date.js
@@ -2,10 +2,10 @@
 function getISODate() {
     var d = new Date();
     var r = d.getFullYear() + "-";
-    if (d.getMonth() < 10) {
+    if (d.getMonth() < 9) {
         r += '0';
     }
-    r += d.getMonth() + "-";
+    r += (d.getMonth() + 1) + "-";
     if (d.getDate() < 10) {
         r += '0';
     }
